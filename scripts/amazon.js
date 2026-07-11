7720a2ff8e4b2f367fcb0e9d6bd5d918a72fa6d5
Khairul Bashar <khairul.b23306@gmail.com>
2026-07-11 21:30:30 +0530

    make the caart qauntity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,5 +73,10 @@ document.querySelectorAll(".add-to-cart-button").forEach((button) => {
                 quantity:1
             });
         }
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
     });
 });
