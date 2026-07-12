8aaba2282282358f63aefecf3c3c7157cfa85fd2
Khairul Bashar <khairul.b23306@gmail.com>
2026-07-13 00:56:53 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,9 +3,7 @@ import {products,getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import {deliveryOptions,getDeliveryOption} from "../../data/deliveryOptions.js";
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D');
+import {renderPaymentSummary} from "./paymentSummary.js";
 export function renderOrderSummary() {
     let cartSummaryHTML = "";
     cart.forEach((cartItem) => {
@@ -92,6 +90,7 @@ export function renderOrderSummary() {
             removeFromCart(productId);
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+            renderPaymentSummary();
         });
     });
     document.querySelectorAll(".js-delivery-option").forEach((option) => {
@@ -100,6 +99,7 @@ export function renderOrderSummary() {
             const deliveryOptionId = option.dataset.deliveryOptionId;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
