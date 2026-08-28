33859200e592ab39cc550d840350e6b069878099
Khairul Bashar <khairul.b23306@gmail.com>
2026-08-29 02:53:24 +0530

    add jasmine testing framework

---
+++ tests/moneyTest.js
@@ -0,0 +1,28 @@
+import {formatCurency} from '../scripts/utils/money.js';
+console.log('test suite : formatCurrency');
+console.log('converts cents to dollars');
+
+if(formatCurrency(2095)=== '20.95') {
+    console.log('Test passed');
+} else {
+    console.log('Test failed');
+}
+console.log('works with zero cents');
+if(formatCurrency(0)=== '0.00') {
+    console.log('Test passed');
+} else {
+    console.log('Test failed');
+}
+console.log('rounds cents to nearest cent');
+
+if(formatCurrency(2000.5)=== '20.01') {
+    console.log('Test passed');
+} else {
+    console.log('Test failed');
+}
+
+if(formatCurrency(2000.4)=== '20.00') {
+    console.log('Test passed');
+} else {
+    console.log('Test failed');
+}
