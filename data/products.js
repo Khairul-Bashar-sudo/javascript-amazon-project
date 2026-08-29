475feb54d0306f8296391d4b68305deb4b77f103
Khairul Bashar <khairul.b23306@gmail.com>
2026-08-30 01:18:03 +0530

    convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -58,21 +58,39 @@ class clothing extends Product {
 const date=new Date();
 date.toLocaleTimeString();
 */
-export let products = [];
-export function loadProducts(fun) {
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', () => {
-    products = JSON.parse(xhr.response).map((productDetails) => {
-      if (productDetails.type === "clothing") {
-        return new clothing(productDetails);
-      }
-      return new Product(productDetails);
+
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products')
+    .then((response) => response.json())
+    .then((data) => {
+      products = data.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+
     });
-    fun();
-  });
-  xhr.open('GET', 'https://supersimplebackend.dev/products');
-  xhr.send();
+  return promise;
 }
+// loadProductsFetch().then(() => {
+//   console.log(products);
+// });
+  // export let products = [];
+  // export function loadProducts(fun) {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.addEventListener('load', () => {
+  //     products = JSON.parse(xhr.response).map((productDetails) => {
+  //       if (productDetails.type === "clothing") {
+  //         return new clothing(productDetails);
+  //       }
+  //       return new Product(productDetails);
+  //     });
+  //     fun();
+  //   });
+  //   xhr.open('GET', 'https://supersimplebackend.dev/products');
+  //   xhr.send();
+  // }
 
 
 // export const products = [
